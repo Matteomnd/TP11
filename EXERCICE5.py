e49e907c6699561da4d915d2c12df2ad932d9aed
PC Mattéo <matteo.menard@isen-ouest.yncrea.fr>
2020-03-30 18:55:13 +0200

    suite

--- EXERCICE5.py
+++ EXERCICE5.py
@@ -1,3 +1,4 @@
+import numpy as np
 class Matrix :
     def __init__(self,data):
         self.__data = data
@@ -8,4 +9,17 @@ class Matrix :
     def __sub__(self,m1):
         return Matrix(self.__data - m1.__data)
     
-    def
+    def __iadd__(self,m1):
+        self.__data += m1.__data
+
+    def __isub__(self,m1):
+        self.__data -= m1.__data
+
+    def __imul__(self, m1):
+        self.__data *= m1.__data
+
+    def __mul__(self, m1):
+        return Matrix(np.dot(self.__data , m1.__data)
+
+    def __lt__(self,m1):
+
