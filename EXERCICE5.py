5a6249a4d491a5918da2af5aa4eea12fc16b8d13
PC Mattéo <matteo.menard@isen-ouest.yncrea.fr>
2020-03-30 19:10:27 +0200

    fin

--- EXERCICE5.py
+++ EXERCICE5.py
@@ -22,4 +22,21 @@ class Matrix :
         return Matrix(np.dot(self.__data , m1.__data)
 
     def __lt__(self,m1):
+        return np.rank(self.__data)<np.rank(m1.__data)
 
+    def __gt__(self, m1):
+        return np.rank(self.__data)>np.rank(m1.__data)
+
+    def __str__(self):
+        return str(self.__data)
+
+
+matrix_1= np.array([[2,5],[6,7]])
+matrix_2 = np.array([[4,7],[6,2]])
+m1= matrix_1 + matrix_2
+print(m1)
+m2 = matrix_1 - matrix_2
+print(m2)
+m3 = matrix_1 * matrix_2
+print(matrix_1+=matrix_2)
+m6 = m1 > m2
